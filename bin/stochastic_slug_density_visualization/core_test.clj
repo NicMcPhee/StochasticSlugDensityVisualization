1afacf747c3758c9d8e0b2c895a6ef397ba906f6
Nic McPhee <mcphee@morris.umn.edu>
2013-06-09 15:18:47 -0500

    Deleting /bin from the repository since it should have never been committed.

--- bin/stochastic_slug_density_visualization/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns stochastic-slug-density-visualization.core-test
-  (:use clojure.test
-        stochastic-slug-density-visualization.core))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
